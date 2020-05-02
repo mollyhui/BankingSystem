44e2d00ad3f3a2630d31b5da52d36ec943cdee70
Nelson Mei <nelsonmei@Nelsons-MacBook-Pro.local>
2020-05-02 00:11:47 -0400

    add exception handling to initialize tables

--- AppDatabase.java
+++ AppDatabase.java
@@ -12,14 +12,17 @@ public class AppDatabase {
         if (!initialized) {
             initialize();
         }
-
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/BankDB?serverTimezone=UTC", "root", "591Nelson");
-        PreparedStatement customerStatement = connection.prepareStatement(
-                "CREATE TABLE BankDB.customers ( SSN char(9), password varchar(45), fName varchar(45), lName varchar(45), walletAmount double , creditScore int)");
-        customerStatement.executeUpdate();
-        PreparedStatement managerStatement = connection.prepareStatement(
-                "CREATE TABLE BankDB.managers ( SSN char(9), password varchar(45), fName varchar(45), lName varchar(45))");
-        managerStatement.executeUpdate();
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/BankDB?serverTimezone=UTC", "root", "591Nelson");
+            PreparedStatement customerStatement = connection.prepareStatement(
+                    "CREATE TABLE BankDB.customers ( SSN char(9), password varchar(45), fName varchar(45), lName varchar(45), walletAmount double , creditScore int)");
+            customerStatement.executeUpdate();
+            PreparedStatement managerStatement = connection.prepareStatement(
+                    "CREATE TABLE BankDB.managers ( SSN char(9), password varchar(45), fName varchar(45), lName varchar(45))");
+            managerStatement.executeUpdate();
+        } catch (SQLSyntaxErrorException e) {
+            System.out.println("already initialized");
+        }
 
     }
 
