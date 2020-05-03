36e6f596eef391fcbb1497fa47fe2fbd576d627c
Nelson Mei <nelsonmei@Nelsons-MacBook-Pro.local>
2020-05-02 23:24:43 -0400

    textfile code for CreateCustomer and UserExists

--- AppBackup.java
+++ AppBackup.java
@@ -2,11 +2,12 @@ import java.io.*;
 
 public class AppBackup {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, Exception {
         if (args[0].equals("a")) {
             System.out.println(authenticateUser("customer", "123456789", "pw123"));
             System.out.println(authenticateUser("manager", "45678765", "3999"));
-
+        } else if (args[0].equals("b")) {
+            createCustomer("555699876", "pw6969", "Christine Poppy", 799);
         }
     }
     // csv file Customer column structure:
@@ -17,15 +18,15 @@ public class AppBackup {
 
     /**
      * methods to have:
-     *  create CSV files - called in GUI code?
+     *  X create CSV files - called in GUI code?
      *  delete CSV files
      *
      *  get Method for all attributes given SSN (customer and manager)
      *  mutate Method for all attributes given SSN (customer and manager)
      *  createCustomer
      *  createManager
-     *  authenticate customer
-     *  authenticate manager
+     *  DONE authenticate customer
+     *  DONE authenticate manager
      *  accountExists (Checking , Saving, Sec)
      *  addAccount (Checking , Saving, Sec)
      *  loanExists
@@ -51,6 +52,39 @@ public class AppBackup {
             }
         }
         return false;
+    }
+
+    public static boolean userExists(String managerOrCustomer, String ssn) throws IOException {
+        BufferedReader csvReader = new BufferedReader(new FileReader("customerData.txt"));
+        if (managerOrCustomer == "manager") {
+            csvReader = new BufferedReader(new FileReader("managerData.txt"));
+        }
+        String row;
+        while ((row = csvReader.readLine()) != null) {
+            String[] data = row.split(",");
+            if (data[0].equals(ssn)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void createCustomer(String ssn, String password, String name, int creditScore) throws Exception {
+        if (userExists("customer", ssn)) {
+            throw new Exception();
+        } else {
+            FileWriter csvWriter = new FileWriter("customerData.txt", true);
+            csvWriter.append(ssn);
+            csvWriter.append(",");
+            csvWriter.append(password);
+            csvWriter.append(",");
+            csvWriter.append(name);
+            csvWriter.append(",");
+            csvWriter.append(Integer.toString(creditScore));
+            csvWriter.append("\n");
+            csvWriter.flush();
+            csvWriter.close();
+        }
     }
 
 
