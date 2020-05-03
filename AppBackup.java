7a7d23238bba9264d661a35f9c03e7d13ed99be8
Nelson Mei <nelsonmei@Nelsons-MacBook-Pro.local>
2020-05-03 00:10:21 -0400

    textfile code for customerAccountExists (savings, etc) and update customer columns

--- AppBackup.java
+++ AppBackup.java
@@ -7,9 +7,13 @@ public class AppBackup {
             System.out.println(authenticateUser("customer", "123456789", "pw123"));
             System.out.println(authenticateUser("manager", "45678765", "3999"));
         } else if (args[0].equals("b")) {
-            createCustomer("555699876", "pw6969", "Christine Poppy", 799);
+            createCustomer("45676543456", "pw6969", "SammyTo", 799);
         } else if (args[0].equals("c")) {
             createManager("92284732", "pwokok", "NelsonBobby");
+        } else if (args[0].equals("d")) {
+            System.out.println(customerAccountExists("45676543456", "checkings"));
+            System.out.println(customerAccountExists("45676543456", "savings"));
+            System.out.println(customerAccountExists("45676543456", "security"));
         }
     }
     // csv file Customer column structure:
@@ -21,12 +25,12 @@ public class AppBackup {
     /**
      * methods to have:
      *  X create CSV files - called in GUI code?
-     *  delete CSV files
+     *  X delete CSV files
      *
      *  get Method for all attributes given SSN (customer and manager)
      *  mutate Method for all attributes given SSN (customer and manager)
-     *  createCustomer
-     *  createManager
+     *  DONE createCustomer
+     *  DONE createManager
      *  DONE authenticate customer
      *  DONE authenticate manager
      *  accountExists (Checking , Saving, Sec)
@@ -83,6 +87,34 @@ public class AppBackup {
             csvWriter.append(name);
             csvWriter.append(",");
             csvWriter.append(Integer.toString(creditScore));
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
+            csvWriter.append(",");
+            csvWriter.append("null");
             csvWriter.append("\n");
             csvWriter.flush();
             csvWriter.close();
@@ -105,4 +137,36 @@ public class AppBackup {
         }
     }
 
+    public static boolean customerAccountExists(String ssn, String accountType) throws IOException {
+        boolean accountExists = false;
+
+        BufferedReader csvReader = new BufferedReader(new FileReader("customerData.txt"));
+        String row;
+        while ((row = csvReader.readLine()) != null) {
+            String[] data = row.split(",");
+            if (data[0].equals(ssn)) {
+                switch (accountType) {
+                    case "checkings": {
+                        if (!data[4].equals("null")) {
+                            accountExists = true;
+                        }
+                        break;
+                    }
+                    case "savings": {
+                        if (!data[7].equals("null")) {
+                            accountExists = true;
+                        }
+                        break;
+                    }
+                    case "security": {
+                        if (!data[11].equals("null")) {
+                            accountExists = true;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return accountExists;
+    }
 }
