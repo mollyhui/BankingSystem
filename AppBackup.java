992a7aaeabf700f0af8bfe91393736288a4cc8a1
Nelson Mei <nelsonmei@Nelsons-MacBook-Pro.local>
2020-05-04 01:01:53 -0400

    textfile code for update user attribute

--- AppBackup.java
+++ AppBackup.java
@@ -3,10 +3,36 @@ import java.util.ArrayList;
 
 public class AppBackup {
 
+    static int numberOfCustomerColumns = 17;
+    static int numberOfManagerColumns = 4;
+
+    static int customerSSNIndex = 0;
+    static int customerPasswordIndex = 1;
+    static int customerNameIndex = 2;
+    static int checkingsAccountNumberIndex = 3;
+    static int checkingsBalanceIndex = 4;
+    static int checkingsTransactionFeeIndex = 5;
+    static int savingsAccountNumberIndex = 6;
+    static int savingsBalanceIndex = 7;
+    static int savingsTransactionFeeIndex = 8;
+    static int savingsInterestIndex = 9;
+    static int securityAccountNumberIndex = 10;
+    static int securityBalanceIndex = 11;
+    static int securityTransactionFeeIndex = 12;
+    static int loanAmountIndex = 13;
+    static int loanTermIndex = 14;
+    static int loanInterestIndex = 15;
+    static int loanUnpaidMonthsIndex = 16;
+
+    static int managerSSNIndex = 0;
+    static int managerPasswordIndex = 1;
+    static int managerNameIndex = 2;
+    static int managerBalanceIndex = 3;
+
     public static void main(String[] args) throws IOException, Exception {
         if (args[0].equals("a")) {
-            System.out.println(authenticateUser("customer", "123456789", "pw123"));
-            System.out.println(authenticateUser("manager", "45678765", "3999"));
+            System.out.println(authenticateUser("customer", "45676543458", "psw6969"));
+            System.out.println(authenticateUser("manager", "6", "change"));
         } else if (args[0].equals("b")) {
             createCustomer("45676543456", "pw6969", "SammyTo");
             createCustomer("45676543457", "pw6969", "SammyTo2");
@@ -20,17 +46,18 @@ public class AppBackup {
         } else if (args[0].equals("e")) {
             System.out.println(customerLoanExists("45676543456"));
         } else if (args[0].equals("f")) {
-            deleteUserFromFile("5", "customer");
             deleteUserFromFile("5", "manager");
         } else if (args[0].equals("g")) {
-            addCheckingsAccount("45676543457", "56785678", 556.26, 2.75);
+            addCheckingsAccount("45676543456", "56785678", 556.26, 2.75);
         } else if (args[0].equals("h")) {
-            addSavingsAccount("45676543457", "56785678", 1987, 2.45, 1.9);
+            addSavingsAccount("45676543456", "56785678", 1987, 2.45, 1.9);
         } else if (args[0].equals("i")) {
-            addSecurityAccount("45676543457", "56785678", 1000, 9.9);
+            addSecurityAccount("45676543456", "56785678", 1000, 9.9);
         } else if (args[0].equals("j")) {
             System.out.println(getUserAttribute("45676543457", "customer", 4));
             System.out.println(getUserAttribute("6", "manager", 1));
+        } else if (args[0].equals("k")) {
+            updateUserAttribute("6", "manager", 1, "change1");
         }
 
 
@@ -51,7 +78,7 @@ public class AppBackup {
      *  NOT NEEDED create CSV files - blank files will be provided (customerData.txt, managerData.txt)
      *
      *  DONE GET Method for all attributes given SSN (customer and manager)
-     *  update Method for all attributes given SSN (customer and manager)
+     *  DONE update Method for all attributes given SSN (customer and manager)
      *  DONE createCustomer
      *  DONE createManager
      *  DONE authenticate customer
@@ -73,7 +100,7 @@ public class AppBackup {
         String row;
         while ((row = csvReader.readLine()) != null) {
             String[] data = row.split(",");
-            if (data[0].equals(ssn)) {
+            if (data[customerSSNIndex].equals(ssn)) {
                 if (data[1].equals(attemptPassword)) {
                     return true;
                 } else {
@@ -93,7 +120,7 @@ public class AppBackup {
         String row;
         while ((row = csvReader.readLine()) != null) {
             String[] data = row.split(",");
-            if (data[0].equals(ssn)) {
+            if (data[customerSSNIndex].equals(ssn)) {
                 return true;
             }
         }
@@ -103,7 +130,7 @@ public class AppBackup {
 
     public static void createCustomer(String ssn, String password, String name) throws Exception {
         if (userExists("customer", ssn)) {
-            throw new Exception();
+            throw new Exception("user already exists");
         } else {
             FileWriter csvWriter = new FileWriter("customerData.txt", true);
             csvWriter.append(ssn);
@@ -148,7 +175,7 @@ public class AppBackup {
 
     public static void createManager(String ssn, String password, String name, double startingBalance) throws Exception {
         if (userExists("manager", ssn)) {
-            throw new Exception();
+            throw new Exception("user already exists");
         } else {
             FileWriter csvWriter = new FileWriter("managerData.txt", true);
             csvWriter.append(ssn);
@@ -172,22 +199,22 @@ public class AppBackup {
         String row;
         while ((row = csvReader.readLine()) != null) {
             String[] data = row.split(",");
-            if (data[0].equals(ssn)) {
+            if (data[customerSSNIndex].equals(ssn)) {
                 switch (accountType) {
                     case "checkings": {
-                        if (!data[4].equals("null")) {
+                        if (!data[checkingsAccountNumberIndex].equals("null")) {
                             accountExists = true;
                         }
                         break;
                     }
                     case "savings": {
-                        if (!data[7].equals("null")) {
+                        if (!data[savingsAccountNumberIndex].equals("null")) {
                             accountExists = true;
                         }
                         break;
                     }
                     case "security": {
-                        if (!data[11].equals("null")) {
+                        if (!data[securityAccountNumberIndex].equals("null")) {
                             accountExists = true;
                         }
                         break;
@@ -206,8 +233,8 @@ public class AppBackup {
         String row;
         while ((row = csvReader.readLine()) != null) {
             String[] data = row.split(",");
-            if (data[0].equals(ssn)) {
-                if (!data[14].equals("null")) {
+            if (data[customerSSNIndex].equals(ssn)) {
+                if (!data[loanAmountIndex].equals("null")) {
                     loanExists = true;
                 }
             }
@@ -220,22 +247,21 @@ public class AppBackup {
         if (!userExists("customer", ssn)) {
             throw new Exception("customer does not exist");
         }
-        String[] userData = new String[17]; // prepare original user data
+        String[] userData = new String[numberOfCustomerColumns]; // prepare original user data
 
         BufferedReader csvReader = new BufferedReader(new FileReader("customerData.txt"));
         String row;
         while ((row = csvReader.readLine()) != null) {
             String[] data = row.split(",");
-            if (data[0].equals(ssn)) {
+            if (data[customerSSNIndex].equals(ssn)) {
                 userData = data;
             }
         }
         // now we have the original user data
         // now update the data to include checkings account info
-        userData[3] = accountNumber;
-        userData[4] = Double.toString(startingBalance);
-        userData[5] = Double.toString(transactionFee);
-        //System.out.println(userData[4]);
+        userData[checkingsAccountNumberIndex] = accountNumber;
+        userData[checkingsBalanceIndex] = Double.toString(startingBalance);
+        userData[checkingsTransactionFeeIndex] = Double.toString(transactionFee);
 
         deleteUserFromFile(ssn, "customer");
         FileWriter csvWriter = new FileWriter("customerData.txt", true);
@@ -255,22 +281,22 @@ public class AppBackup {
         if (!userExists("customer", ssn)) {
             throw new Exception("customer does not exist");
         }
-        String[] userData = new String[17]; // prepare original user data
+        String[] userData = new String[numberOfCustomerColumns]; // prepare original user data
 
         BufferedReader csvReader = new BufferedReader(new FileReader("customerData.txt"));
         String row;
         while ((row = csvReader.readLine()) != null) {
             String[] data = row.split(",");
-            if (data[0].equals(ssn)) {
+            if (data[customerSSNIndex].equals(ssn)) {
                 userData = data;
             }
         }
         // now we have the original user data
         // now update the data to include savings account info
-        userData[6] = accountNumber;
-        userData[7] = Double.toString(startingBalance);
-        userData[8] = Double.toString(transactionFee);
-        userData[9] = Double.toString(interest);
+        userData[savingsAccountNumberIndex] = accountNumber;
+        userData[savingsBalanceIndex] = Double.toString(startingBalance);
+        userData[savingsTransactionFeeIndex] = Double.toString(transactionFee);
+        userData[savingsInterestIndex] = Double.toString(interest);
 
         deleteUserFromFile(ssn, "customer");
         FileWriter csvWriter = new FileWriter("customerData.txt", true);
@@ -290,21 +316,21 @@ public class AppBackup {
         if (!userExists("customer", ssn)) {
             throw new Exception("customer does not exist");
         }
-        String[] userData = new String[17]; // prepare original user data
+        String[] userData = new String[numberOfCustomerColumns]; // prepare original user data
 
         BufferedReader csvReader = new BufferedReader(new FileReader("customerData.txt"));
         String row;
         while ((row = csvReader.readLine()) != null) {
             String[] data = row.split(",");
-            if (data[0].equals(ssn)) {
+            if (data[customerSSNIndex].equals(ssn)) {
                 userData = data;
             }
         }
         // now we have the original user data
         // now update the data to include security account info
-        userData[10] = accountNumber;
-        userData[11] = Double.toString(startingBalance);
-        userData[12] = Double.toString(transactionFee);
+        userData[securityAccountNumberIndex] = accountNumber;
+        userData[securityBalanceIndex] = Double.toString(startingBalance);
+        userData[securityTransactionFeeIndex] = Double.toString(transactionFee);
 
         deleteUserFromFile(ssn, "customer");
         FileWriter csvWriter = new FileWriter("customerData.txt", true);
@@ -342,6 +368,60 @@ public class AppBackup {
     }
 
 
+    public static void updateUserAttribute(String ssn, String managerOrCustomer, int indexOAttribute, String newValue) throws Exception {
+        if (!userExists("customer", ssn) && (!userExists("manager", ssn))) {
+            throw new Exception("User does not exist");
+        }
+
+        String[] userData;
+        if (managerOrCustomer.equals("customer")) {
+            userData = new String[numberOfCustomerColumns]; // prepare original user data
+        } else {
+            userData = new String[numberOfManagerColumns];
+        }
+
+        BufferedReader csvReader;
+        if (managerOrCustomer.equals("customer")) {
+            csvReader = new BufferedReader(new FileReader("customerData.txt"));
+        } else {
+            csvReader = new BufferedReader(new FileReader("managerData.txt"));
+        }
+
+        String row;
+        while ((row = csvReader.readLine()) != null) {
+            String[] data = row.split(",");
+            if (data[managerOrCustomer.equals("customer") ? customerSSNIndex : managerSSNIndex].equals(ssn)) {
+                userData = data;
+            }
+        }
+        // now update the data to reflect new value
+        userData[indexOAttribute] = newValue;
+
+        if (managerOrCustomer.equals("customer")) {
+            deleteUserFromFile(ssn, "customer");
+        } else {
+            deleteUserFromFile(ssn, "manager");
+        }
+
+        FileWriter csvWriter;
+        if (managerOrCustomer.equals("customer")) {
+            csvWriter = new FileWriter("customerData.txt", true);
+        } else {
+            csvWriter = new FileWriter("managerData.txt", true);
+        }
+
+        for (int i=0; i<userData.length; i++) {
+            csvWriter.append(userData[i]);
+            if (i != userData.length-1) {
+                csvWriter.append(",");
+            }
+        }
+        csvWriter.append("\n");
+        csvWriter.flush();
+        csvWriter.close();
+    }
+
+
     /**
      * helper method used for replacing row in txt file
      */
@@ -357,7 +437,7 @@ public class AppBackup {
         String row;
         while ((row = csvReader.readLine()) != null) {
             String[] data = row.split(",");
-            if (! data[0].equals(ssn)) {
+            if (! data[customerOrManager.equals("customer") ? customerSSNIndex : managerSSNIndex].equals(ssn)) {
                 putBack.add(data);
             }
         }
